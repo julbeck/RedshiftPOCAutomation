eaedbb02c73d1078eb1d7b7e7c9501ce239c72b4
Kakli <kaklis@38f9d352a275.ant.amazon.com>
2021-04-30 10:21:58 -0400

    modified:   sctconfig.sh

--- sctconfig.sh
+++ sctconfig.sh
@@ -4,7 +4,7 @@ cd /awsutilities
 mkdir SCT
 curl -o aws-schema-conversion-tool-1.0.latest.zip  https://s3.amazonaws.com/publicsctdownload/Fedora/aws-schema-conversion-tool-1.0.latest.zip
 unzip aws-schema-conversion-tool-1.0.latest.zip
-yum -y install aws-schema-conversion-tool-1.0.649-1.x86_64.rpm
+yum -y install aws-schema-conversion-tool-*.rpm
 yum -y install java-11-amazon-corretto
 mkdir /usr/local/jdbc-drivers
 aws s3 cp s3://event-driven-app-with-lambda-redshift/scripts/mssql-jdbc-7.4.1.jre8.jar /usr/local/jdbc-drivers/mssql-jdbc-7.4.1.jre8.jar
