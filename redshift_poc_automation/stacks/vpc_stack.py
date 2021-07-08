0b5c8eb718ce9b87f706482920b25cf1fd6026ac
julbeck <78858798+julbeck@users.noreply.github.com>
2021-07-08 09:39:00 -0400

    Update vpc_stack.py

--- redshift_poc_automation/stacks/vpc_stack.py
+++ redshift_poc_automation/stacks/vpc_stack.py
@@ -75,6 +75,13 @@ class VpcStack(core.Stack):
             value=f"{self.vpc.vpc_default_security_group.security_group_id}",
             description="Default security group of this VPC."
         )
+        
+        output_2 = core.CfnOutput(
+            self,
+            "Test output",
+            value="It got here",
+            description="Testing whether it got here"
+        )
 
     # properties to share with other stacks
     @property
