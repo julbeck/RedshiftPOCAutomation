3fa6a8879e4591d46880e9b988845e4045ec1178
julbeck <78858798+julbeck@users.noreply.github.com>
2021-07-13 15:15:14 -0400

    Update app.py

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ glue_crawler_s3_config = config.get('glue_crawler_s3')
 # VPC Stack for hosting secure API & other resources
 vpc_stack = VpcStack(
     app,
-    f"{os.env.STACK_NAME}-vpc-stack",
+    f"{os.getenv('STACK_NAME')}-vpc-stack",
     env=env,
     vpc_id=vpc_id,
     vpc_config=vpc_config,
