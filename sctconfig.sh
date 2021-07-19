f060d0b8515fa3d8540354c28ea9db31e353f0a7
kaklisamir <81388407+kaklisamir@users.noreply.github.com>
2021-07-19 19:19:29 -0400

    Add files via upload

--- sctconfig.sh
+++ sctconfig.sh
@@ -8,14 +8,13 @@ curl -o aws-schema-conversion-tool-1.0.latest.zip  https://s3.amazonaws.com/publ
 tar -xf aws-schema-conversion-tool-1.0.latest.zip
 msiexec.exe /i "AWS Schema Conversion Tool-1.0.652.msi" /passive /l log.txt
 curl -o AWSCLIV2.msi https://awscli.amazonaws.com/AWSCLIV2.msi
-msiexec.exe /i "AWSCLIV2.msi" /passive /l logcli.txt
 set PATH="c:\Program Files\Amazon\AWSCLIV2\";%PATH%
-aws configure set role_arn arn:aws:iam::396171519679:role/windows-cli-admin
+aws configure set role_arn arn:aws:iam::962393875414:role/windows-cli-role
 aws configure set credential_source Ec2InstanceMetadata
 aws s3 cp s3://event-driven-app-with-lambda-redshift/scripts/sctcliauto.scts sctcliauto.scts
 aws s3 cp s3://event-driven-app-with-lambda-redshift/scripts/sctrun.sh sctrun.sh
-mkdir Drivers
-cd Drivers
-aws s3 cp s3://event-driven-app-with-lambda-redshift/scripts/mssql-jdbc-7.4.1.jre8.jar mssql-jdbc-7.4.1.jre8.jar
-aws s3 cp s3://event-driven-app-with-lambda-redshift/scripts/redshift-jdbc42-2.0.0.4.jar redshift-jdbc42-2.0.0.4.jar
+#mkdir Drivers
+#cd Drivers
+#aws s3 cp s3://event-driven-app-with-lambda-redshift/scripts/mssql-jdbc-7.4.1.jre8.jar mssql-jdbc-7.4.1.jre8.jar
+#aws s3 cp s3://event-driven-app-with-lambda-redshift/scripts/redshift-jdbc42-2.0.0.4.jar redshift-jdbc42-2.0.0.4.jar
 </script>
