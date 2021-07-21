5d9a289bc8c6bb23838c5cdc19d82f5348406a73
julbeck <78858798+julbeck@users.noreply.github.com>
2021-07-21 14:15:44 -0400

    Update sctconfig_2.sh

--- sctconfig_2.sh
+++ sctconfig_2.sh
@@ -1,6 +1,6 @@
 aws configure set credential_source Ec2InstanceMetadata
 aws s3 cp s3://event-driven-app-with-lambda-redshift/scripts/sctcliauto.scts sctcliauto.scts
-aws s3 cp s3://event-driven-app-with-lambda-redshift/scripts/sctrun.sh sctrun.sh
+aws s3 cp s3://event-driven-app-with-lambda-redshift/scripts/sctrun.ps1 sctrun.ps1
 mkdir Drivers
 cd Drivers
 aws s3 cp s3://event-driven-app-with-lambda-redshift/scripts/mssql-jdbc-7.4.1.jre8.jar mssql-jdbc-7.4.1.jre8.jar
